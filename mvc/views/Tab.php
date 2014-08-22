d7a14631d4e04ccddf31ed02935453cc1e87d459
rich@richjenks.com <rich@richjenks.com>
2014-08-22 15:45:11 +0100

    Formatting

--- mvc/views/Tab.php
+++ mvc/views/Tab.php
@@ -2,7 +2,5 @@
 
 require __DIR__ . DS . 'partials' . DS . 'Header.php';
 require __DIR__ . DS . 'partials' . DS . 'Menu.php';
-foreach ( $data as $table ) {
-	require __DIR__ . DS . 'partials' . DS . 'Table.php';
-}
+foreach ( $data as $table ) require __DIR__ . DS . 'partials' . DS . 'Table.php';
 require __DIR__ . DS . 'partials' . DS . 'Footer.php';
